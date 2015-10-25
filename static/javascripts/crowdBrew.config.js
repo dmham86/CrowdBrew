67c0eef5d912a437ca597035eeaf7da9c4a6a55a
David Hamilton <dmham@udel.edu>
2015-10-25 09:43:46 -0400

    More fixes for the annoying authentication issues.

--- static/javascripts/crowdBrew.config.js
+++ static/javascripts/crowdBrew.config.js
@@ -5,14 +5,32 @@
     .module('crowdBrew.config')
     .config(config);
 
-  config.$inject = ['$locationProvider'];
+  config.$inject = ['$locationProvider', '$provide', '$httpProvider'];
 
   /**
   * @name config
   * @desc Enable HTML5 routing
   */
-  function config($locationProvider) {
+  function config($locationProvider, $provide, $httpProvider) {
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
+    registerInterceptor($provide, $httpProvider);
+  }
+
+  function registerInterceptor($provide, $httpProvider) {
+    // register the interceptor as a service
+    $provide.factory('myHttpInterceptor', function($q) {
+      return {
+        'responseError': function(rejection) {
+          if(rejection.status == 401){
+            // Redirect to homepage, will clear out auth token
+            window.location = "/";
+          }
+          return $q.reject(rejection);
+        }
+      };
+    });
+
+    $httpProvider.interceptors.push('myHttpInterceptor');
   }
 })();
